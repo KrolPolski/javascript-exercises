46f1663ccb6f3f02367ea2bd5fbcd48b11957da8
KrolPolski <ryanboudwin@outlook.com>
2023-04-09 13:42:41 +0300

    passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removedItem) {
+    const index = array.indexOf(removedItem);
+    trimmed = array.splice(index,1);
+    return array;
 };
-
+console.log(removeFromArray([1,2,3],2));
 // Do not edit below this line
 module.exports = removeFromArray;
