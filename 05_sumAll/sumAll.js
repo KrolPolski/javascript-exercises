6cad211df09d325c995d0bba85444ba83c337565
Ryan Boudwin <ryanboudwin@outlook.com>
2023-04-09 17:13:20 +0300

    sumall passes first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
 
-};
-
+const sumAll = function(firstNum, lastNum) {
+    let runningTotal = 0;
+for (let i= lastNum - firstNum + 1; i > 0; i--)
+{
+    runningTotal += i;
+    
+}
+return runningTotal;}
+console.log(sumAll(1,5));
 // Do not edit below this line
 module.exports = sumAll;
