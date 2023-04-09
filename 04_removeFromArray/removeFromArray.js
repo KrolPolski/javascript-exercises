b9a8d8e3f484369a059173718abafdfe70e9eb3a
Ryan Boudwin <ryanboudwin@outlook.com>
2023-04-09 16:20:50 +0300

    remove from array exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function(array, removedItem) {
-    const index = array.indexOf(removedItem);
-    trimmed = array.splice(index,1);
-    return array;
-};
-console.log(removeFromArray([1,2,3],2));
+const removeFromArray = function(array, ...args) {
+    
+        return array.filter(x => !args.includes(x))
+    };
+
+console.log(removeFromArray([1,2,3],2,1));
 // Do not edit below this line
 module.exports = removeFromArray;
