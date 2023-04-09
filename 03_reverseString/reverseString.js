dd1a40ad388ba51ff186cd64b93eed908f89ca45
KrolPolski <ryanboudwin@outlook.com>
2023-04-09 13:28:19 +0300

    reverse string exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+let length = input.length;
+let reversedOutput ="";
+for (let i=length-1; i >=0; i--)
+{
+    reversedOutput += input[i];
+}
 
+return reversedOutput;
 };
 
 // Do not edit below this line
