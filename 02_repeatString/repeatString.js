070079338d1d517fdbef7efc0b058804c0626265
KrolPolski <ryanboudwin@outlook.com>
2023-04-09 13:02:01 +0300

    repeatstring exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,11 @@
 const repeatString = function(string, num) {
     let repeatedString = "";
+    if (num >= 0){
     for (let i = 0; i < num; i++){
      repeatedString += string;          
     }
-    return repeatedString;
+    return repeatedString;}
+    else return "ERROR";
 };
 repeatString('hey',3);
 // Do not edit below this line
