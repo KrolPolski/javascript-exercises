798cfac7118bbfc7f37c097571d2cd7cd0aeaacc
KrolPolski <ryanboudwin@outlook.com>
2023-04-12 13:46:36 +0300

    getTheTitles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+// books is the name of the Array.
+// title: is a property of the array. 
+const map1 = array.map(a => a.title);
+return map1;
 };
 
 // Do not edit below this line
