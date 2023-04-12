ab518ae5c85b8fb0fd9a43da9d090688d0c17edd
KrolPolski <ryanboudwin@outlook.com>
2023-04-12 12:18:51 +0300

    palindrome exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function (input) {
+// purge punctuation, white space and make everything lowercase
 
-};
+let lowerInput = input.toLowerCase(); 
+let splitInputArray = lowerInput.split('');
+let sanitizedArray = splitInputArray.filter(function(letter){
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+    return punctuation.indexOf(letter) === -1;
+    
+console.log(sanitizedArray);
+
+});
+let reunitedInput = sanitizedArray.join();
+return reunitedInput === reverseString(reunitedInput);
+
+}
+
+
+    
+    
+console.log(palindromes("Hannah!!!..."));
+function reverseString(input) 
+    {
+    let length = input.length;
+    let reversedOutput ="";
+    for (let i=length-1; i >=0; i--)
+    {
+        reversedOutput += input[i];
+    }
+    
+    return reversedOutput;
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
