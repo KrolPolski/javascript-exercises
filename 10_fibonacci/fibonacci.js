e352da60bfd65c018c1e4e154d8fac05a9e3a893
KrolPolski <ryanboudwin@outlook.com>
2023-04-12 13:03:15 +0300

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+input = parseInt(input);
+if (input >= 0){
+    let fibonacciArray = [0,1];
 
-};
+    for (let i = 1; i < input; i++) {
+        let newItem = fibonacciArray[i] + fibonacciArray[i-1];    
+        console.log(newItem);
+        fibonacciArray.push(newItem);
+}
+return fibonacciArray[input];
+}
+else return "OOPS";
+}
 
+
+console.log(fibonacci(5));
 // Do not edit below this line
 module.exports = fibonacci;
