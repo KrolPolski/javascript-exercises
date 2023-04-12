03e877042251679d121b4395162c51e0e4542798
KrolPolski <ryanboudwin@outlook.com>
2023-04-12 10:43:57 +0300

    calculator exercise complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const { run } = require("jest-cli");
+
+const add = function(a, b) {
+	return a + b;
+   
+};
+console.log(add(1,2));
+const subtract = function(a,b) {
+	return a -b;};
+
+const sum = function(array) {
+return array.reduce(add, 0);
+};
+
+const multiply = function(array) {
+return array.reduce((a,i) => a * i);
+}
+const power = function(a,b) {
+  return a ** b; 
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  else if (a < 0) return Error;
+  else {
+    let runningTotal = a;
+    for (let i = a - 1; i > 0; i--) {
+           runningTotal = runningTotal * i;
+    }
+    return runningTotal;
+  }
 };
 
 // Do not edit below this line
