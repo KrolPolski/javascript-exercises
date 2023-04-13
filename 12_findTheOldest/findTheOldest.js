8cf69017edf98d876a14b7d3107d1c5e18503f96
KrolPolski <ryanboudwin@outlook.com>
2023-04-13 12:43:42 +0300

    gettheOldest exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,41 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array)
+ {
+  let currentYear = new Date().getFullYear();
+  array.forEach(element => 
+    {
+      if(element.yearOfDeath != null)
+      {
+        element.Age = element.yearOfDeath - element.yearOfBirth;
+      }
+      else
+      {
+        element.Age = currentYear - element.yearOfBirth;
+      }
+    });
+  array.sort((a,b) => b.Age - a.Age);
+  return array[0];
+}
+/* this was for testing
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+console.log(findTheOldest(people));
+console.table(people);
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
